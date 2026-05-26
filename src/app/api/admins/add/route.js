416adfc43e5e6c4a342eeb65ea96f0a5568d8fd0
krish <krishkachhadiya20@gmail.com>
2026-05-26 16:01:13 +0530

    update add user api with server side validation

--- src/app/api/admins/add/route.js
+++ src/app/api/admins/add/route.js
@@ -6,9 +6,6 @@ import {
   writeData,
 } from "@/lib/filehandler";
 
-
-
-
 // ======================
 // ADD USER
 // ======================
@@ -22,62 +19,143 @@ export async function POST(
     const body =
       await req.json();
 
+    let {
+      name,
+      email,
+      password,
+      role,
+      permissions,
+    } = body;
 
+    // ======================
+    // VALIDATION
+    // ======================
 
+    if (
+      !name ||
+      !email ||
+      !password ||
+      !role
+    ) {
 
+      return NextResponse.json(
+        {
+          success: false,
+
+          message:
+            "All fields are required",
+        },
+        { status: 400 }
+      );
+    }
+
+    // ======================
+    // CLEAN VALUES
+    // ======================
+
+    name =
+      name.trim();
+
+    email =
+      email
+        .trim()
+        .toLowerCase();
+
+    password =
+      password.trim();
+
+    role =
+      role.trim();
+
+    // ======================
     // READ ADMINS
+    // ======================
 
     const admins =
       readData(
         "admins.json"
       );
 
+    // ======================
+// CHECK EMAIL OR NAME EXISTS
+// ======================
 
+const alreadyExists =
+  admins.find(
+    (item) =>
 
+      item.email
+        ?.toLowerCase() ===
+        email.toLowerCase() ||
 
+      item.name
+        ?.toLowerCase() ===
+        name.toLowerCase()
+  );
+
+if (
+  alreadyExists
+) {
+
+  return NextResponse.json(
+    {
+      success: false,
+
+      message:
+        "Email or username already exists",
+    },
+    { status: 400 }
+  );
+}
+
+    // ======================
     // CREATE USER
+    // ======================
 
     const newAdmin = {
 
       id: Date.now(),
 
-      name: body.name,
+      name,
 
-      email: body.email,
+      email,
 
-      password:
-        body.password,
+      password,
 
-      role: body.role,
+      role,
 
       permissions:
-        body.permissions,
+        permissions || {},
     };
 
 
-
-
+    // ======================
     // PUSH USER
+    // ======================
 
     admins.push(
       newAdmin
     );
 
-
-
-
+    // ======================
     // SAVE FILE
+    // ======================
 
     writeData(
       "admins.json",
       admins
     );
 
-
-
+    // ======================
+    // RESPONSE
+    // ======================
 
     return NextResponse.json({
+
       success: true,
+
+      message:
+        "User Added Successfully",
 
       admin:
         newAdmin,
@@ -90,6 +168,7 @@ export async function POST(
     return NextResponse.json(
       {
         success: false,
+
         message:
           "Server Error",
       },
