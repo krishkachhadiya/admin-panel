5559bd71a2df7c7cb47fab26b2e86aa7102ead92
krish <krishkachhadiya20@gmail.com>
2026-05-27 10:56:03 +0530

    add a api for product edit and delete api

---
+++ src/app/api/products/[id]/route.js
@@ -0,0 +1,175 @@
+import { NextResponse } from "next/server";
+
+import {
+  readData,
+  writeData,
+} from "@/lib/filehandler";
+
+
+
+// ======================
+// DELETE PRODUCT
+// ======================
+
+export async function DELETE(
+  req,
+  { params }
+) {
+  try {
+    const { id } = await params;
+
+    const products =
+      readData("products.json");
+
+    const filteredProducts =
+      products.filter(
+        (item) =>
+          item.id != id
+      );
+
+    writeData(
+      "products.json",
+      filteredProducts
+    );
+
+    return NextResponse.json({
+      success: true,
+      message:
+        "Product Deleted Successfully",
+    });
+  } catch (error) {
+    console.log(error);
+
+    return NextResponse.json(
+      {
+        success: false,
+        message:
+          "Internal Server Error",
+      },
+      { status: 500 }
+    );
+  }
+}
+
+
+
+// ======================
+// UPDATE PRODUCT
+// ======================
+
+export async function PUT(
+  req,
+  context
+) {
+
+  try {
+
+    const params =
+      await context.params;
+
+    const id =
+      params.id;
+
+    const body =
+      await req.json();
+
+
+
+
+    // READ PRODUCTS
+
+    const products =
+      readData(
+        "products.json"
+      );
+
+
+
+
+    // FIND PRODUCT
+
+    const productIndex =
+      products.findIndex(
+        (item) =>
+          String(item.id) ===
+          String(id)
+      );
+
+
+
+
+    // NOT FOUND
+
+    if (
+      productIndex === -1
+    ) {
+
+      return NextResponse.json(
+        {
+          success: false,
+          message:
+            "Product Not Found",
+        },
+        { status: 404 }
+      );
+    }
+
+
+
+
+    // UPDATE PRODUCT
+
+    products[
+      productIndex
+    ] = {
+      ...products[
+        productIndex
+      ],
+
+      ...body,
+
+      updatedAt:
+        new Date().toISOString(),
+    };
+
+
+
+
+    // SAVE
+
+    writeData(
+      "products.json",
+      products
+    );
+
+
+
+
+    // RESPONSE
+
+    return NextResponse.json({
+      success: true,
+
+      message:
+        "Product Updated Successfully",
+
+      product:
+        products[
+          productIndex
+        ],
+    });
+
+  } catch (error) {
+
+    console.log(error);
+
+    return NextResponse.json(
+      {
+        success: false,
+        message:
+          "Internal Server Error",
+      },
+      { status: 500 }
+    );
+  }
+}
