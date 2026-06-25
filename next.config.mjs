8cf578ff9bd9bc838380a7f5dcdacaba3fb8652c
krish <krishkachhadiya20@gmail.com>
2026-06-25 12:56:01 +0530

    allow to image path

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-  
+  allowedDevOrigins: ['lotas.local', '192.168.1.7'],
+  images: { // Changed "Images" to lowercase "images"
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
-  
